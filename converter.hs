3a19368435f92d268c3180deb6efb43dcfe86035
David Bergström <david-bergstrom@outlook.com>
2015-08-24 13:03:49 +0200

    Add OverloadedStrings

--- converter.hs
+++ converter.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 import Data.Text.IO
 import qualified Data.Text as T
 import Prelude hiding (writeFile, readFile, putStrLn, lines, concat)
@@ -9,22 +11,19 @@ organization e = e !! 5
 team e = e !! 6
 
 splitEntry line = T.split (=='*') line
-convertEntryToLatex e = T.concat [(T.pack "\\makebadge{"),
-                                  name e, (T.pack "}{"), team e, (T.pack "}{"),
-                                  organization e, (T.pack "}{"),
-                                  role e, (T.pack "}{"), color e, (T.pack "}")]
+convertEntryToLatex e = T.concat [("\\makebadge{"), name e, "}{", team e, "}{",
+                                  organization e, "}{", role e, "}{", color e, "}"]
 
-escapedCharacters :: [(T.Text, T.Text)]
-escapedCharacters = [(T.pack "#", T.pack "\\#"),
-                     (T.pack "$", T.pack "\\$"),
-                     (T.pack "%", T.pack "\\%"),
-                     (T.pack "&", T.pack "\\&"),
-                     (T.pack "\"", T.pack "\\textbackslash{}"),
-                     (T.pack "^", T.pack "\\textasciicircum{}"),
-                     (T.pack "_", T.pack "\\_"),
-                     (T.pack "{", T.pack "\\{"),
-                     (T.pack "}", T.pack "\\}"),
-                     (T.pack "~", T.pack "\\textasciitilde{}")]
+escapedCharacters = [("#",  "\\#"),
+                     ("$",  "\\$"),
+                     ("%",  "\\%"),
+                     ("&",  "\\&"),
+                     ("\"", "\\textbackslash{}"),
+                     ("^",  "\\textasciicircum{}"),
+                     ("_",  "\\_"),
+                     ("{",  "\\{"),
+                     ("}",  "\\}"),
+                     ("~",  "\\textasciitilde{}")]
 
 escapeForLatex s = foldr f s escapedCharacters
   where f (x, y) acc = T.replace x y acc
